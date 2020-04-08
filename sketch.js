b4682d06fecfd578a9a66673b166c67e7a62b819
gabrielchi <55122175+gabrielchi@users.noreply.github.com>
2020-04-08 22:05:53 +0800

    circle MOVE

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function preload() {
 
 function setup() {
   createCanvas(400, 400);
+  angleMode(DEGREES);
   button = createButton('toggle');
   button.mousePressed(toggleSong);
   song.play();
@@ -28,16 +29,20 @@ function draw() {
   background(35);
   var vol = amp.getLevel();
   volhist.push(vol);
+  
+  stroke(255);
   noFill();
-
+  translate(width / 2, height / 2);
   beginShape();
-  for (var i = 0; i < volhist.length; i++){
-    var y = map(volhist[i], 0, 1, height, 0);
-    vertex(i, y);
+  for (var i = 0; i < 360; i++){
+    var r = map(volhist[i], 0, 1, 10, 100);
+    var x = r * cos(i);
+    var y = r * sin(i);
+    vertex(x, y);
   }
   endShape();
 
-  if (volhist.length > width){
+  if (volhist.length > 360){
     volhist.splice(0,1);
   }
 
